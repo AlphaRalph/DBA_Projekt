de4f6614af6f9a9f3137666c7ab20e03c692818a
Aeon <aeon_sake@hotmail.com>
2018-03-22 10:26:44 +0100

    test

---
+++ DBA_Projekt/Main.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+namespace DBA_Projekt
+{
+    public partial class Main : Form
+    {
+        #region constructor
+        public Main() => InitializeComponent();
+        #endregion
+    }
+}
