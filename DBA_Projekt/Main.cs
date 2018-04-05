aeefddf1deb103e072e5eac0e2a370ecfdc2eee6
AlphaRalph <AlphaRalph@users.noreply.github.com>
2018-04-05 10:06:53 +0200

    comments added to main

--- DBA_Projekt/Main.cs
+++ DBA_Projekt/Main.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace DBA_Projekt
 {
@@ -12,8 +13,23 @@ namespace DBA_Projekt
     /// </summary>
     public partial class Main : Form
     {
+
+        //public Main()
+        //{
+        //    MySqlConnection conn = new MySqlConnection("SERVER=192.168.142.128;" + "DATABASE=fhooe;" + "UID=fhooe" + "PASSWORD=1234;");
+        //    conn.Open();
+        //    InitializeComponent();
+        //}
+
         #region constructor
         public Main() => InitializeComponent();
         #endregion
+
+
+        //DataTable table = new DataTable();
+        //myDataAdapter.Fill(table);
+        //BindingSource sourc = new BindeingSource();
+        //sourc.DataSource = table;
+        //dataGridView.DataSource = bindingSource;
     }
 }
